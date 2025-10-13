9a840500b79c3962efad1dcf15f3559eaf7adc74
MulubahzumuKSipor <sip23001@byui.edu>
2025-10-13 22:22:51 +0000

    Week5

--- server/db.ts
+++ server/db.ts
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
     port: Number(process.env.DB_PORT),
 })
 
